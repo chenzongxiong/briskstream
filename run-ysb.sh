fe7f9b57b522516a35750bb8395d184424290024
zongxiong.chen <czxczf@gmail.com>
2019-09-13 10:15:07 +0200

    add YSB script

---
+++ run-ysb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+OPTS="-server -XX:+UseConcMarkSweepGC -XX:NewRatio=2 -XX:SurvivorRatio=16 -Xms8g -Xmx8g"
+JCP=".:./BriskBenchmarks:./BriskBenchmarks/target/classes"
+JCP="${JCP}:BriskBenchmarks/target/BriskBenchmarks-1.2.0-jar-with-dependencies.jar"
+CLASS="applications.BriskRunner"
+
+java $OPTS -cp $JCP $CLASS --app YSB $@
